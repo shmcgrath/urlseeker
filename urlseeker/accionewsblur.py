a234ab5e161fe4d55e6e5d579aaee53604c8d18f
Sarah H. McGrath <sarah@shmcgrath.com>
2019-08-30 17:54:57 -0400

    Clean code to use fstrings and bookmarks classes

--- urlseeker/accionewsblur.py
+++ urlseeker/accionewsblur.py
@@ -1,45 +1,42 @@
 # -*- coding: UTF-8 -*-
 
-import requests
+import bookmark
+from collections import Mapping
+import datetime
 import json
 import logging
 import os
-import sys
-import bookmark
-from collections import Mapping
 from pathlib import Path
+import requests
+import sys
 
-newsblurUrl = 'https://www.newsblur.com'
+NEWSBLUR_URL = "https://www.newsblur.com"
 
 def login(newsblurUser, newsblurPass):
     global newsblurSessionId
     global newsblurCookies
-    loginUrl = newsblurUrl + '/api/login'
-    loginParams = {'username': newsblurUser, 'password': newsblurPass}
-
+    loginUrl = f"{NEWSBLUR_URL}/api/login"
+    loginParams = {"username": newsblurUser, "password": newsblurPass}
     login = requests.post(loginUrl, data=loginParams)
-    logging.info('==LOGIN INFORMATION==')
-    logging.info('Login Status Code: ' + str(login.status_code))
+    logging.info("==LOGIN INFORMATION==")
+    logging.info(f"Login Status Code: {login.status_code}")
     logging.info(login.json())
 
-    newsblurSessionId = login.cookies['newsblur_sessionid']
-    logging.info('newsblur session id: ' + newsblurSessionId)
+    newsblurSessionId = login.cookies["newsblur_sessionid"]
+    logging.info(f"newsblurSessionId: {newsblurSessionId}")
     newsblurCookies = dict(newsblur_sessionid=str(newsblurSessionId))
-    logging.info('newsblurCookies:')
-    logging.info(newsblurCookies)
-
-    return newsblurSessionId
+    logging.info(f"newsblurCookies: {newsblurCookies}")
 
 def starred_count():
     starredCount = 0
 
-    readerUrl = newsblurUrl + '/reader/feeds'
-    readerParams = {'flat': 'true'}
+    readerUrl = f"{NEWSBLUR_URL}/reader/feeds"
+    readerParams = {"flat": "true"}
 
     reader = requests.get(readerUrl, params=readerParams, cookies=newsblurCookies)
     readerRaw = reader.json()
 
-    starredCount = readerRaw['starred_count']
+    starredCount = readerRaw["starred_count"]
 
     return starredCount
 
@@ -48,19 +45,19 @@ def starred_pages():
     starredPages = starredCount // 10
     starredRemainder = starredCount % 10
 
-    if starredRemainder !=0:
+    if starredRemainder != 0:
         starredPages+=2
 
-    if starredRemainder ==0:
+    if starredRemainder == 0:
         starredPages+=1
 
     return starredPages
 
 def get_bookmark_detail(starredStory):
-    title = starredStory['story_title']
-    url = starredStory['story_permalink']
-    tags = starredStory['story_tags']
-    folders = starredStory['user_tags']
+    title = starredStory["story_title"]
+    url = starredStory["story_permalink"]
+    tags = starredStory["story_tags"]
+    folders = starredStory["user_tags"]
 
     newBookmark = bookmark.Bookmark(title, url);
 
@@ -76,28 +73,29 @@ def get_bookmark_detail(starredStory):
 
 # MAKE IF CHECK LOOKING TO SEE IF THE STORY COUNT IS 0
 def get_starred_stories():
+    logging.debug("hit get_starred_stories")
     home = str(Path.home())
-    logging.debug('hit get_starred_stories')
+    today = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
     currentPage = 1
     starredPages = starred_pages()
 
-    storiesUrl = newsblurUrl + '/reader/starred_stories'
+    storiesUrl = f"{NEWSBLUR_URL}/reader/starred_stories"
 
-    netscapeBookmarks = bookmark.create_html_file(home, 'newsblur')
+    netscapeFile = bookmark.HtmlFile(f"{home}/{today}-bookmarks.html")
+    netscapeFile.create_file()
 
     while currentPage < starredPages:
-        storiesParams = {'page': currentPage,}
+        storiesParams = {"page": currentPage,}
         stories = requests.get(storiesUrl, params=storiesParams,
                 cookies=newsblurCookies)
         storiesRaw = stories.json()
-        userStarredStories = storiesRaw['stories']
+        userStarredStories = storiesRaw["stories"]
 
-        print('Gathering starred stories from page ' + str(currentPage) + '.')
+        print(f"Gathering starred stories from page {currentPage}.")
         for starredStory in userStarredStories:
             newBookmark = get_bookmark_detail(starredStory)
-            bookmark.write_html_bookmark(netscapeBookmarks,
-                    newBookmark.title, newBookmark.url, newBookmark.tagString)
+            netscapeFile.write_bookmark(newBookmark)
 
-            currentPage+=1
+        currentPage+=1
 
-    bookmark.write_html_footer(netscapeBookmarks)
+    netscapeFile.write_footer()
