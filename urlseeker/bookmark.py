64b9187ced3462fdf6c5f1f307a3a281d2fa66be
Sarah H. McGrath <sarah@shmcgrath.com>
2019-08-30 17:48:59 -0400

    fix fstrings in bookmarks; add dts default value

--- urlseeker/bookmark.py
+++ urlseeker/bookmark.py
@@ -4,6 +4,7 @@ import datetime
 
 class Bookmark:
     """ Bookmark class containing the title, url, and tags of a bookmark."""
+
     def __init__(self, title, url):
         self.title = title
         self.url = url
@@ -21,75 +22,100 @@ class Bookmark:
 
 class HtmlFile:
     """HtmlFile docstring"""
-    def __init__(self, filePath):
-        self.filePath = filePath
-        self.fileObject = open(self.filePath, "w")
+
+    def __init__(self, file_path, current_dts=None):
+        self.file_path = file_path
+        if current_dts is None:
+            current_dts = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
+        self.current_dts = current_dts
+        self.file_object = open(self.file_path, "w")
 
     def create_file(self):
         """create_file creates the HTML bookmark file at the path of the
         HtmlFile object."""
 
-        currentDTS = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
-        print("Creating Netscape HTML Bookmarks file at " + self.filePath)
-        self.fileObject.write(f"<!DOCTYPE NETSCAPE-Bookmark-file-1>\
-            \n\t<!--This is an automatically generated file.\
-            \n\tIt will be read and overwritten.\
-            \n\tDo Not Edit! -->\
-            \n\t<Title> {currentDTS} Bookmarks</Title>\
-            \n\t<H1> {currentDTS} Bookmarks</H1> \
-            \n\t<DL>")
+        netscape_header = (
+            f"<!DOCTYPE NETSCAPE-Bookmark-file-1>"
+            f"\n\t<!--This is an automatically generated file."
+            f"\n\tIt will be read and overwritten."
+            f"\n\tDo Not Edit! -->"
+            f"\n\t<Title> {self.current_dts} Bookmarks</Title>"
+            f"\n\t<H1> {self.current_dts} Bookmarks</H1>"
+            f"\n\t<DL>"
+        )
+
+        print("Creating Netscape HTML Bookmarks file at {self.file_path}.")
+        self.file_object.write(netscape_header)
 
     def write_footer(self):
         """write_footer docstring"""
-        self.fileObject.write("\n\t</DL>")
-        self.fileObject.close()
+
+        netscape_footer = (
+            f"\n\t</DL>"
+        )
+
+        self.file_object.write(netscape_footer)
+        self.file_object.close()
 
     def write_bookmark(self, bookmark):
         """write_bookmark docstring"""
-        title = bookmark.title
-        url = bookmark.url
-        tags = bookmark.tagString
-        htmlLine = f'\n\t<DT><A HREF="{url}" TAGS="{tags}">{title}</A>'
-        self.fileObject.write(htmlLine)
+
+        netscape_line = (
+            f'\n\t<DT><A HREF="{bookmark.url}" TAGS="{bookmark.tagString}">'
+            f'{bookmark.title}</A>'
+        )
+
+        self.file_object.write(netscape_line)
 
 class MarkdownFile:
     """MarkdownFile docstring"""
-    def __init__(self, filePath, bookmarkSource):
-        self.filePath = filePath
-        self.bookmarkSource = bookmarkSource
-        self.fileObject = open(self.filePath, "w")
+    def __init__(self, file_path, bookmark_source, current_dts=None):
+        self.file_path = file_path
+        self.bookmark_source = bookmark_source
+        if current_dts is None:
+            current_dts = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
+        self.current_dts = current_dts
+        self.file_object = open(self.file_path, "w")
 
     def create_file(self):
         """create_file creates the markdown bookmark file at the path of the
         MarkdownFile object."""
 
-        currentDTS = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
-        print("Creating Markdown Bookmarks file at " + mdFile)
-        self.fileObject.write(f"# {currentDTS} {self.bookmarkSource} Bookmarks\n \
-                ## Bookmarks\n")
+        markdown_header = (
+            f"# {self.current_dts} {self.bookmark_source} Bookmarks"
+            f"\n## Bookmarks\n"
+        )
 
+        print("Creating Markdown Bookmarks file at {self.file_path}.")
+        self.file_object.write(markdown_header)
 
     def write_footer(self):
         """write_footer docstring"""
-        currentDTS = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
-        self.fileObject.write(f"\n## Bookmark File Information \
-            \n- Created On: {currentDTS} \
-            \n- Source: {self.bookmarkSource} \
-            \n- Created by: \
-            [urlseeker](https://github.com/shmcgrath/urlseeker)")
-        self.fileObject.close()
+
+        markdown_footer=(
+            f"\n## Bookmark File Information"
+            f"\n- Created On: {self.current_dts}"
+            f"\n- Source: {self.bookmark_source}"
+            f"\n- Created by: [urlseeker](https://github.com/shmcgrath/urlseeker)"
+        )
+
+        self.file_object.write(markdown_footer)
+        self.file_object.close()
 
     def write_bookmark(self, bookmark):
         """write_bookmark docstring"""
-        title = bookmark.title
-        url = bookmark.url
-        tags = bookmark.tagString
-        markdownLine = f"\n- [{title}]({url})\ttags: {tags}"
-        self.fileObject.write(markdownLine)
+
+        markdown_line = (
+            f"\n- [{bookmark.title}]({bookmark.url})"
+            f"\ttags: {bookmark.tagString}"
+        )
+
+        self.file_object.write(markdown_line)
 
 def main():
-    print("This file only contains class definitions and should not be \
-            run directly.")
+    print(
+        f"This file contains class definitions and should not be run directly."
+    )
 
 if __name__ == "__main__":
     main()
