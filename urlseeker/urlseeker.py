5539c48ef0c08c6e2747a0c8c2e7130b4011a2a8
Sarah H. McGrath <sarah@shmcgrath.com>
2019-08-30 14:42:20 -0400

    Initial setup of CLI args with argparse.
    I still need to figure out a way to handle running all but the argued
    service. This will take some thinking. I may use store_false for -A.

--- urlseeker/urlseeker.py
+++ urlseeker/urlseeker.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+
+""" Module Docstring """
 
 import requests
 import json
 import os
 import sys
-import bookmarkfiles
-import accionewsblur
-import accioreddit
+import bookmark
+#import accionewsblur
+#import accioreddit
+import argparse
 from collections import Mapping
 import logging
 from logging.config import fileConfig
@@ -34,7 +38,9 @@ def account_information():
         print('error importing account information from accountinfo.py')
         exit(1)
 
-    if not redditUsername or not redditPassword or not redditClientId or not redditClientSecret or not redditUserAgent or not newsblurUsername or not newsblurPassword:
+    if (not redditUsername or not redditPassword or not redditClientId or
+        not redditClientSecret or not redditUserAgent or not newsblurUsername or
+        not newsblurPassword):
         logging.info('hitting the not')
         exit(1)
 
@@ -56,13 +62,69 @@ def main():
     logging.debug('main called')
     user = account_information();
     logging.debug('main finished')
-    #TODO: gather args about what services i should be pulling down when i run
-    # the script (all, reddit, newsblur, twitter, pinboard, hacker news)
-    #TODO: add a flag for various output types (bookmark, to pinboard, csv, markdown)
-    #accionewsblur.login(user['newsblurUsername'], user['newsblurPassword'])
-    #accionewsblur.get_starred_stories()
-    redditAccessToken = accioreddit.login(user['redditUsername'], user['redditPassword'], user['redditClientId'], user['redditClientSecret'], user['redditUserAgent'])
-    accioreddit.get_saved_stories(user['redditUsername'], user['redditUserAgent'], redditAccessToken)
+    parser = argparse.ArgumentParser(description="Move urls between \
+            local files and various online services.")
+    parser.add_argument("-t", "--type", action="store", type=str,
+            choices=["html", "markdown", "pinboard"],
+            help="Set where to store or output  retrieved bookmarks. \
+                    The default output is an HTML Netscape bookmark file.\n \
+                    The following words  are accetped as arguments for this \
+                    flag:\n- 'html': outputs bookmarks to a local HTML file. \
+                    \n- 'md': outputs bookmarks to a local markdown file. \
+                    \n- 'pinboard': sends retrieved bookmarks to \
+                    Pinboard.in.\nIf Pinboard is the destination and a source, \
+                    all non-Pinboard bookmarks will go to Pinboard and all \
+                    Pinboard bookmarks will be pulled to an HTML bookmark file.")
+    # TODO: add CSV output support
+    parser.add_argument("-o", "--output", action="store", type=str,
+            help="Set output file location and name of bookmarks file. \
+                    This string should be the complete path and file name \
+                    without a filetype / extensiton. \
+                    The default file location and name is \
+                    '~/YYYY.MM.DD-urlseek-bookmarks.{ext}' where 'ext' is \
+                    the filetype of the --type choice (if a file is created. \
+                    There is no need to pass in a file extension, as one will \
+                    be automatically added. If a file extension/type is \
+                    passed in as part of the path, it will be ignored and the \
+                    filetype will be added to the end of the string.")
+    parser.add_argument("-A", "--all", action="store_true",
+            help="run urlseeker for all services \
+                    this is the default behavior.")
+    #TODO: if -A is given do all but any ones that are also designated
+    parser.add_argument("-H", "--hackernews", action="store_true",
+            help="run urlseeker for hacker news")
+    parser.add_argument("-N", "--newsblur", action="store_true",
+            help="run urlseeker for newsblur")
+    parser.add_argument("-P", "--pinboard", action="store_true",
+            help="run urlseeker for pinboard. If this option is selected and \
+                    the output type is pinboard, the pinboard bookmarks will \
+                    not be re-sent to pinboard. Instead, all pinboard \
+                    bookmarks will be pulled to a bookmark HTML file.")
+    # TODO: add date range support for pulling down pinboard bookmarks
+    parser.add_argument("-R", "--reddit", action="store_true",
+            help="run urlseeker for reddit")
+    parser.add_argument("-T", "--twitter", action="store_true",
+            help="run urlseeker for twitter")
+    args = parser.parse_args()
+    logging.debug(args)
+    if args.newsblur:
+        logging.debug("newsblur = true")
+        """
+        accionewsblur.login(user['newsblurUsername'], user['newsblurPassword'])
+        accionewsblur.get_starred_stories()
+        """
+    if args.reddit:
+        logging.debug("reddit = true")
+        """
+        redditAccessToken = accioreddit.login(user['redditUsername'],
+            user['redditPassword'], user['redditClientId'],
+            user['redditClientSecret'], user['redditUserAgent'])
+        accioreddit.get_saved_stories(user['redditUsername'],
+                user['redditUserAgent'], redditAccessToken)
+        """
+    if (not args.all and not args.hackernews and not args.newsblur and
+            not args.pinboard and not args.reddit and not args.twitter):
+        logging.debug("-AHNPRT not set. This will run all.")
 
 if __name__ == '__main__':
     main()
