4416e656f4f895a22d254d4c814f2e1d7dccfc28
Sarah H. McGrath <sarah@shmcgrath.com>
2019-08-30 23:08:04 -0400

    Move all icloud tabs into main method

--- urlseeker/accioicloudtabs.py
+++ urlseeker/accioicloudtabs.py
@@ -1,53 +1,36 @@
 # -*- coding: UTF-8 -*-
 
-import os
+import bookmark
+from collections.abc import Mapping
 import datetime
+import logging
+import os
+from pathlib import Path
 import sqlite3
 from sqlite3 import Error
-import logging
 import sys
-from collections.abc import Mapping
-from pathlib import Path
-import bookmarkfiles
-
-def get_today_string():
-    today = datetime.datetime.now().strftime('%Y-%m-%d-%H.%M')
-    return today
-
-def gather_cloud_tabs(conn, cloudTabsBookmarks):
-    cloudTabsSql = 'SELECT cloud_tabs.title title, cloud_tabs.url url FROM cloud_tabs'
-    c = conn.cursor()
-    for row in c.execute(cloudTabsSql):
-        bookmarkfiles.write_html_bookmark(cloudTabsBookmarks, str(row[0]),
-                str(row[1]), "")
-
-def create_connection(db_file):
-    # create a database connection to the SQLite database
-    # specified by the db_file
-    # :param db_file: database file
-    # :return: connection object or None
-    # http://www.sqlitetutorial.net/sqlite-python/sqlite-python-select/
-
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return None
 
 def main():
-    home = str(Path.home())
-    cloudTabsLocation = home + "/Library/Safari/CloudTabs.db"
-    today = bookmarkfiles.get_today_string()
-    htmlFileName = home + f"/{today}-iCloudTabs.html"
-    cloudTabsBookmarks = bookmarkfiles.create_html_file(htmlFileName)
+    CLOUD_TABS_LOCATION = f"{HOME}/Library/Safari/CloudTabs.db"
+    HOME = str(Path.HOME())
+    current_dts = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M")
 
-    conn = create_connection(cloudTabsLocation)
+    netscapeFile = bookmark.htmlFile(f"{HOME}/{current_dts}-iCloudTabs.html")
+
+    try:
+        conn = sqlite3.connect(CLOUD_TABS_LOCATION)
+    except Error as e:
+        print(f"There has been an error: {e}")
+
     with conn:
-        gather_cloud_tabs(conn, cloudTabsBookmarks)
+        cloudTabsSql="SELECT ct.title title, ct.url url FROM cloud_tabs AS ct"
+        c = conn.cursor()
 
-    bookmarkfiles.write_html_footer(cloudTabsBookmarks)
+        for row in c.execute(cloudTabsSql):
+            row_bookmark = bookmark.Bookmark(f"{row[0]}", f"{row[1]}")
+            netscapeFile.write_bookmark(row_bookmark)
+
+    netscapeFile.write_footer()
 
 if __name__ == '__main__':
     main()
