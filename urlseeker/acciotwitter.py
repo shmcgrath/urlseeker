590f63bc138da17372d736c5b4d6389bb14ac00f
Sarah H. McGrath <sarah@shmcgrath.com>
2019-09-01 16:31:53 -0400

    Add user variables to login for twitter

--- urlseeker/acciotwitter.py
+++ urlseeker/acciotwitter.py
@@ -12,6 +12,13 @@ import sys
 
 TWITTER_URL = ""
 TWITTER_OAUTH_URL = ""
+#"twitter_app_id": twitter_app_id,
+#"twitter_api_key": twitter_api_key,
+#"twitter_api_secret_key": twitter_api_secret_key,
+#"twitter_access_token": twitter_access_token,
+#"twitter_access_token_secret": twitter_access_token_secret,
+#"twitter_user": twitter_user,
+#"twitter_passwd": twitter_passwd,
 
-def login():
+def login(user):
     current_dts = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
